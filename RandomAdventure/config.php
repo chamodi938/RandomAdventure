fadeb5efd54a54bc1039f989d006b2155dcdedcf
chamodi938 <136189209+chamodi938@users.noreply.github.com>
2024-11-11 14:45:41 +0530

    Added the references

--- RandomAdventure/config.php
+++ RandomAdventure/config.php
@@ -9,6 +9,7 @@ $db = 'game_db';
 $user = 'root';
 $pass = '';
 
+//code adapted from myCompiler url: https://www.mycompiler.io/view/1PJazAcFJJm
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
